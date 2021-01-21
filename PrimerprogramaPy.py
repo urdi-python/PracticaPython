09c2c9cfc2d6c84df14413aefd6d76c88df41f33
Iván Urdinola <urdinola1991@gmail.com>
2021-01-20 20:27:00 -0500

    Conversor con tres opciones de moneda

--- PrimerprogramaPy.py
+++ PrimerprogramaPy.py
@@ -1,8 +1,35 @@
-pesos = input("Cuántos $$ pesos Colombianos tienes: ")
-pesos = float(pesos)
-valor_dolar = 3489
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $$"+ " " + dolares + " " + "Dólares")
+menu = """
+Bienvenido al conversor de monedas
+1- Pesos Colombianos
+2- Pesos Argentinos
+3- Pesos Mexicanos
+Elige una Opción
+"""
+opcion = int(input(menu))      #ingresamos el valor en texto
 
+if opcion == 1:
+    pesos = input("Cuántos $$ pesos Colombianos tienes: ")
+    pesos = float(pesos)
+    valor_dolar = 3489
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $$"+ " " + dolares + " " + "Dólares")
+elif opcion == 2:
+    pesos = input("Cuántos $$ pesos Argentinos tienes: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $$"+ " " + dolares + " " + "Dólares")
+elif opcion == 3:
+    pesos = input("Cuántos $$ pesos Mexicanos tienes: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $$"+ " " + dolares + " " + "Dólares")
+else:
+    print("Ingresa una opción correcta por favor")
