5833810ac9d18ae2d9434a3a4b290a803e840162
Iván Urdinola <urdinola1991@gmail.com>
2021-01-20 20:16:49 -0500

    Segundo commit con líneas de código Py

---
+++ PrimerprogramaPy.py
@@ -0,0 +1,8 @@
+pesos = input("Cuántos $$ pesos Colombianos tienes: ")
+pesos = float(pesos)
+valor_dolar = 3489
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares = str(dolares)
+print("Tienes $$"+ " " + dolares + " " + "Dólares")
+
