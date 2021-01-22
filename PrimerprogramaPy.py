fd88483e77004ff2f689bd2619ee661715900f88
Iván Urdinola <urdinola1991@gmail.com>
2021-01-21 19:58:04 -0500

    Volví a reescribir codigo base

--- PrimerprogramaPy.py
+++ PrimerprogramaPy.py
@@ -1,8 +1,7 @@
-pesos = input("Cuántos $$ pesos Colombianos tienes: ")
+pesos = input("Cuantos pesos Colombianos tienes: ")
 pesos = float(pesos)
-valor_dolar = 3489
+valor_dolar = 3400
 dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
-print("Tienes $$"+ " " + dolares + " " + "Dólares")
-
+print("Tienes"+ " " + dolares + " " + "Dolares")
